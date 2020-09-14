02fbc0492a1b52a6fe44587c253fe22a2362cb0b
Noama Samreen <samreen@etsu.edu>
2020-09-14 14:30:59 -0400

    composer fixed

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://www.cs.ryerson.ca/~nfsamree/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
